8929035631b9905ba60bbab8732b789d5791c06c
zyf0726 <940104357@qq.com>
2017-05-09 21:37:58 +0800

    change PIL to Pillow

--- sobel.py
+++ sobel.py
@@ -1,5 +1,5 @@
 #-*-coding:utf-8-*-
-import Image
+from PIL import Image
 import sys
 import pprint
 
@@ -12,31 +12,31 @@ pic = im.load()
 width = im.size[0]
 height = im.size[1]
 
-Gx = [[0]*height for x in xrange(width)]
-Gy = [[0]*height for x in xrange(width)]
-G  = [[0]*height for x in xrange(width)]
+Gx = [[0]*height for x in range(width)]
+Gy = [[0]*height for x in range(width)]
+G  = [[0]*height for x in range(width)]
 
 K = 65 #灰度阈值
-for x in xrange(1, width-1):
-	for y in xrange(1, height-1):
+for x in range(1, width-1):
+	for y in range(1, height-1):
 		Gx[x][y] = pic[x+1, y-1] + 2*pic[x+1, y] + pic[x+1, y+1] - \
 				   (pic[x-1, y-1] + 2*pic[x-1, y] + pic[x-1, y+1])
 		Gy[x][y] = pic[x-1, y-1] + 2*pic[x, y-1] + pic[x+1, y-1] - \
 				   (pic[x-1, y+1] + 2*pic[x, y+1] + pic[x+1, y+1])
 		G[x][y] = abs(Gx[x][y]) + abs(Gy[x][y])
 
-'''
-file = open("L.txt", "w")
-for x in xrange(width):
-	for y in xrange(height):
-		print >> file, G[x][y],
-	print >> file, ''
-file.close()
-'''
+
+f = open("L.txt", "w")
+for x in range(width):
+	for y in range(height):
+		print(G[x][y], file = f)
+	print('', file = f)
+f.close()
+
 
 newIm = Image.new("L", (width, height))
-for x in xrange(width):
-	for y in xrange(width):
+for x in range(width):
+	for y in range(width):
 		if (G[x][y] > K):
 			newIm.load()[x, y] = 255
 newIm.show()
