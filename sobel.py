d23e9eda1774a8469879efa779a43e2d05709058
zyf0726 <940104357@qq.com>
2017-05-12 23:47:53 +0800

    edit sobel.py

--- sobel.py
+++ sobel.py
@@ -1,42 +1,35 @@
 #-*-coding:utf-8-*-
 from PIL import Image
 import sys
-import pprint
 
-inputPNG = sys.argv[1]
-outputPNG = sys.argv[2]
+def SobelEdgeDetection(im):
+	im = im.convert('L')
+	f = im.load()
+	width = im.size[0]
+	height = im.size[1]
 
-im = Image.open(inputPNG)
-im = im.convert('L')
-pic = im.load()
-width = im.size[0]
-height = im.size[1]
+	Gx = [[0]*height for x in range(width)]
+	Gy = [[0]*height for x in range(width)]
+	G  = [[0]*height for x in range(width)]
 
-Gx = [[0]*height for x in range(width)]
-Gy = [[0]*height for x in range(width)]
-G  = [[0]*height for x in range(width)]
+	K = 75 # 灰度阈值
+	pt = [] # 边界点
+	for x in range(1, width-1):
+		for y in range(1, height-1):
+			Gx[x][y] = f[x+1, y-1] + 2*f[x+1, y] + f[x+1, y+1] - \
+					   (f[x-1, y-1] + 2*f[x-1, y] + f[x-1, y+1])
+			Gy[x][y] = f[x-1, y-1] + 2*f[x, y-1] + f[x+1, y-1] - \
+					   (f[x-1, y+1] + 2*f[x, y+1] + f[x+1, y+1])
+			G[x][y] = abs(Gx[x][y]) + abs(Gy[x][y])
+			if G[x][y] > K:
+				pt += [(x, y)]
+	return pt
 
-K = 65 #灰度阈值
-for x in range(1, width-1):
-	for y in range(1, height-1):
-		Gx[x][y] = pic[x+1, y-1] + 2*pic[x+1, y] + pic[x+1, y+1] - \
-				   (pic[x-1, y-1] + 2*pic[x-1, y] + pic[x-1, y+1])
-		Gy[x][y] = pic[x-1, y-1] + 2*pic[x, y-1] + pic[x+1, y-1] - \
-				   (pic[x-1, y+1] + 2*pic[x, y+1] + pic[x+1, y+1])
-		G[x][y] = abs(Gx[x][y]) + abs(Gy[x][y])
+if __name__ == '__main__':
+	inputPNG = sys.argv[1]
+	im = Image.open(inputPNG)
 
-
-f = open("L.txt", "w")
-for x in range(width):
-	for y in range(height):
-		print(G[x][y], file = f)
-	print('', file = f)
-f.close()
-
-
-newIm = Image.new("L", (width, height))
-for x in range(width):
-	for y in range(width):
-		if (G[x][y] > K):
-			newIm.load()[x, y] = 255
-newIm.show()
+	newIm = Image.new("L", im.size)
+	for (x, y) in SobelEdgeDetection(im):
+		newIm.load()[x, y] = 255
+	newIm.show()
