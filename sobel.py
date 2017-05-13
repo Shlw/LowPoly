902867a93eddb3df747c4960b5857a90c597557c
zyf0726 <940104357@qq.com>
2017-05-13 23:58:46 +0800

    add function 'SelectVertices'

--- sobel.py
+++ sobel.py
@@ -1,12 +1,12 @@
 #-*-coding:utf-8-*-
 from PIL import Image
 import sys
+import random
 
 def SobelEdgeDetection(im):
     im = im.convert('L')
     f = im.load()
-    width = im.size[0]
-    height = im.size[1]
+    width, height = im.size
 
     Gx = [[0]*height for x in range(width)]
     Gy = [[0]*height for x in range(width)]
@@ -25,11 +25,26 @@ def SobelEdgeDetection(im):
                 pt += [(x, y)]
     return pt
 
+def SelectVertices(im, cnt):
+    pt, ver = SobelEdgeDetection(im), []
+    width, height = im.size
+    ratio = 0.7
+    P1 = (cnt * ratio) / len(pt)
+    P2 = (cnt * (1 - ratio)) / (width * height)
+    for (x, y) in pt:
+        if random.random() <= P1:
+            ver += [(x, y)]
+    for x in range(width):
+        for y in range(height):
+            if random.random() <= P2:
+                ver += [(x, y)]
+    return ver
+
 if __name__ == '__main__':
     inputPNG = sys.argv[1]
     im = Image.open(inputPNG)
 
     newIm = Image.new("L", im.size)
-    for (x, y) in SobelEdgeDetection(im):
+    for (x, y) in SelectVertices(im, 2000):
         newIm.load()[x, y] = 255
     newIm.show()
